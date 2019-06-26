1d534b5ae6d1d1f80df4ab165e6c6f775a6f353d
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2019-06-25 20:44:00 -0500

    fix: exports

--- packages/access-decision-manager-express/src/index.ts
+++ packages/access-decision-manager-express/src/index.ts
@@ -1,4 +1,5 @@
 import AccessDecisionManagerProvider from './access-decision-manager-provider';
+import isGrantedMiddleware from './is-granted-middleware';
 
-export * from './is-granted-middleware';
 export default AccessDecisionManagerProvider;
+export { isGrantedMiddleware }
