7a562a4f3105de119903d786035ca23951f53588
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2019-07-12 12:57:43 -0500

    feat: export adm from adm-express

--- packages/access-decision-manager-express/src/index.ts
+++ packages/access-decision-manager-express/src/index.ts
@@ -1,5 +1,6 @@
+import AccessDecisionManager, { Voter } from '@wizeline/access-decision-manager';
 import AccessDecisionManagerProvider from './access-decision-manager-provider';
 import isGrantedMiddleware from './is-granted-middleware';
 
 export default AccessDecisionManagerProvider;
-export { isGrantedMiddleware }
+export { isGrantedMiddleware, AccessDecisionManager, Voter }
