f906c6b8c5bafa92d5dcb59e2ab491daf3ed44ee
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2020-02-20 16:41:03 -0600

    feat(react): add bindind for react
    re #28

---
+++ packages/access-decision-manager-react/src/index.ts
@@ -0,0 +1,6 @@
+import AccessDecisionManager, { Voter } from '@wizeline/access-decision-manager';
+import AccessDecisionManagerProvider, { useIsGranted } from './access-decision-manager-provider';
+
+
+export default AccessDecisionManagerProvider;
+export { useIsGranted, AccessDecisionManager, Voter}
