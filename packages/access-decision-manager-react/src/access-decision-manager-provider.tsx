f05a750e23f5669a07c58cb3eb5217be6c66400c
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2020-02-24 19:55:54 -0600

    improvement(react): implement useEffect to handle async logic
    The hook useIsGranted might use async logic that needs to be resolved before returnuing a result but
    hooks doesn't work well with `async await` so a new local state management was introduce to handle
    ot its logic that is watching `accessDecisionManager`, `attribute`, and `subject`

--- packages/access-decision-manager-react/src/access-decision-manager-provider.tsx
+++ packages/access-decision-manager-react/src/access-decision-manager-provider.tsx
@@ -1,23 +1,16 @@
-import React, {createContext, useContext} from 'react';
+import React, {createContext} from 'react';
 import AccessDecisionManager, {Voter} from '@wizeline/access-decision-manager';
 
 interface AccessDecisionManagerContextType {
   accessDecisionManager: AccessDecisionManager;
 }
 
-const accessDecisionManagerContext = createContext<AccessDecisionManagerContextType>({});
-
+export const accessDecisionManagerContext = createContext<AccessDecisionManagerContextType>({});
 
 const AccessDecisionManagerProvider = ({voters, children, user}: {voters: Voter[], children: any, user: any}) => {
   const accessDecisionManager = new AccessDecisionManager(user, voters, null);
 
   return <accessDecisionManagerContext.Provider value={accessDecisionManager}>{children}</accessDecisionManagerContext.Provider>
-};
-
-export const useIsGranted = async (attribute:any, subject: any) => {
-  const  accessDecisionManager  = useContext(accessDecisionManagerContext);
-  const isGranted = await accessDecisionManager.isGranted(attribute, subject);
-  return isGranted
 };
 
 export default AccessDecisionManagerProvider;
