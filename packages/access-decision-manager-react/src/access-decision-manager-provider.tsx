a5e644ffc85ce311665f11d0dc535f52329e88e0
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2020-02-26 18:15:29 -0600

    improvement(react): add createContext attribute and add context type support

--- packages/access-decision-manager-react/src/access-decision-manager-provider.tsx
+++ packages/access-decision-manager-react/src/access-decision-manager-provider.tsx
@@ -5,12 +5,28 @@ interface AccessDecisionManagerContextType {
   accessDecisionManager: AccessDecisionManager;
 }
 
-export const accessDecisionManagerContext = createContext<AccessDecisionManagerContextType>({});
+export const accessDecisionManagerContext = createContext<AccessDecisionManagerContextType>({
+  accessDecisionManager: new AccessDecisionManager({}, [], null)
+});
 
-const AccessDecisionManagerProvider = ({voters, children, user}: {voters: Voter[], children: any, user: any}) => {
-  const accessDecisionManager = new AccessDecisionManager(user, voters, null);
+// eslint-disable-next-line no-shadow
+const AccessDecisionManagerProvider = ({children, user, createContext, voters,}
+: {
+  children: any,
+  createContext?: any
+  user: any,
+  voters: Voter[],
+}) => {
+  const context =
+    typeof createContext === 'function'
+  ? createContext() : null;
 
-  return <accessDecisionManagerContext.Provider value={accessDecisionManager}>{children}</accessDecisionManagerContext.Provider>
+  const accessDecisionManager = new AccessDecisionManager(user, voters, context);
+  const value = {
+    accessDecisionManager
+  };
+
+  return <accessDecisionManagerContext.Provider value={value}>{children}</accessDecisionManagerContext.Provider>
 };
 
 export default AccessDecisionManagerProvider;
