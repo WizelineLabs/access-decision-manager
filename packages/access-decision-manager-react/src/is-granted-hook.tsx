4e243fb0ee8a84c83f483a4ea11179dfb25c8e7f
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2020-02-27 13:42:08 -0600

    chore(react): npm i dev dependencies

---
+++ packages/access-decision-manager-react/src/is-granted-hook.tsx
@@ -0,0 +1,55 @@
+import {useContext, useEffect, useReducer} from 'react';
+import { accessDecisionManagerContext } from './access-decision-manager-provider';
+
+const initialState = {
+    error: undefined,
+    isGranted: undefined,
+    loading: true,
+};
+
+function reducer(state: any, action: any) {
+    switch (action.type) {
+        case 'error':
+            return {
+                error: action.error,
+                isGranted: undefined,
+                loading: false,
+            };
+        case 'request':
+            return initialState;
+        case 'response':
+            return {
+                error: undefined,
+                isGranted: action.isGranted,
+                loading: false,
+            };
+
+        default:
+            throw new Error();
+    }
+}
+
+const useIsGranted = (attribute:any, subject?: any) => {
+    const  context = useContext(accessDecisionManagerContext);
+    const [state, dispatch] = useReducer(reducer, initialState);
+    useEffect
+    ( () => {
+            const isGrantedDispatcher = async () => {
+                dispatch({ type: 'request'});
+
+                try{
+                    const isGranted = await context.accessDecisionManager.isGranted(attribute, subject);
+                    dispatch({type: 'response', isGranted})
+                } catch (error) {
+                    dispatch({type: 'error', error})
+                }
+            };
+            isGrantedDispatcher();
+        },
+        [context.accessDecisionManager, attribute, subject]
+    );
+
+    return state;
+};
+
+export default useIsGranted;
