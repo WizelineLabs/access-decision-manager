6b11850bc7e69cbf6fd8e4101fc7440c32f34f21
alfredo.granja <ext-a.granja@rappi.com>
2021-10-25 14:41:43 -0500

    add tests

--- packages/access-decision-manager-react/src/__tests__/access-decision-manager-provider.test.tsx
+++ packages/access-decision-manager-react/src/__tests__/access-decision-manager-provider.test.tsx
@@ -1,38 +1,128 @@
-import React from 'react'
+import React, { useContext, useRef, useState } from 'react';
 import AccessDecisionManager from '@wizeline/access-decision-manager';
-import AccessDecisionManagerProvider from '../access-decision-manager-provider';
+import AccessDecisionManagerProvider, {
+  AccessDecisionManagerContext,
+} from '../access-decision-manager-provider';
+import testRenderer, { act } from 'react-test-renderer';
+
+const MockConsumer = () => {
+  const context = useContext(AccessDecisionManagerContext);
+  return (
+    <div>
+      <h1 id="context-value">{JSON.stringify(context)}</h1>;
+    </div>
+  );
+};
+const mockVoters = [];
+const mockUser = {
+  id: 1,
+};
 
 describe('src', () => {
   describe('access-decision-manager-react', () => {
     describe('access-decision-manager-provider', () => {
-      it('adds `accessDecisionManager` to the context', () => {
-        const mockUser = {
-          id: 1,
-        };
-        const mockVoters = [];
-        const component = AccessDecisionManagerProvider({voters: mockVoters, user: mockUser, children: null});
-        expect(component.props.value.accessDecisionManager).toBeInstanceOf(AccessDecisionManager);
-      })
+      it('provides `accessDecisionManager` with null context if none specified', () => {
+        const tree = testRenderer.create(
+          <AccessDecisionManagerProvider user={mockUser} voters={mockVoters}>
+            <MockConsumer />
+          </AccessDecisionManagerProvider>,
+        );
+
+        const initialADM = new AccessDecisionManager(
+          mockUser,
+          mockVoters,
+          null,
+        );
+
+        const context = tree.root.findByProps({ id: 'context-value' });
+        expect(context.props.children).toBe(JSON.stringify(initialADM));
+      });
 
       it('allows you to provide a context factory', () => {
         const context = Symbol('context');
-        const mockUser = {
-          id: 1,
-        };
-        const mockVoters = [];
         const mockContextFactory = jest.fn().mockReturnValue(context);
-        const component = AccessDecisionManagerProvider({voters: mockVoters, user: mockUser, children: null, createContext: mockContextFactory});
 
-        expect(component.props.value.accessDecisionManager.context).toBe(context);
-      })
+        const tree = testRenderer.create(
+          <AccessDecisionManagerProvider
+            user={mockUser}
+            voters={mockVoters}
+            createContext={mockContextFactory}
+          >
+            <MockConsumer />
+          </AccessDecisionManagerProvider>,
+        );
 
-      it('provides a null context if any is specified', () => {
-        const mockUser = {}
-        const mockVoters = [];
-        const component = AccessDecisionManagerProvider({voters: mockVoters, user: mockUser, children: null});
+        const initialADM = new AccessDecisionManager(
+          mockUser,
+          mockVoters,
+          context,
+        );
 
-        expect(component.props.value.accessDecisionManager.context).toBe(null);
-      })
-    })
-  })
+        const node = tree.root.findByProps({ id: 'context-value' });
+        expect(mockContextFactory).toBeCalled();
+        expect(node.props.children).toBe(JSON.stringify(initialADM));
+      });
+
+      it('updates `accessDecisionManager` on props changed', () => {
+        let mountCounter = 1;
+        const MockConsumerRenderCount = () => {
+          const context = useContext(AccessDecisionManagerContext);
+          const id = useRef(mountCounter++);
+          return (
+            <div>
+              <h1 data-testid="context-value" id="context-value">
+                {JSON.stringify(context)}
+              </h1>
+              ;
+              <div data-testid="mount-counter" id="mount-counter">
+                {id.current}
+              </div>
+            </div>
+          );
+        };
+
+        const initialADM = new AccessDecisionManager(
+          mockUser,
+          mockVoters,
+          null,
+        );
+
+        const updatedADM = new AccessDecisionManager(
+          { id: 2 },
+          [...mockVoters, { id: 3 }],
+          null,
+        );
+
+        let tree;
+        act(() => {
+          tree = testRenderer.create(
+            <AccessDecisionManagerProvider user={mockUser} voters={mockVoters}>
+              <MockConsumerRenderCount />
+            </AccessDecisionManagerProvider>,
+          );
+        });
+
+        const context = tree.root.findByProps({ id: 'context-value' });
+        expect(context.props.children).toBe(JSON.stringify(initialADM));
+
+        act(() => {
+          tree.update(
+            <AccessDecisionManagerProvider
+              user={{ id: 2 }}
+              voters={[...mockVoters, { id: 3 }]}
+            >
+              <MockConsumerRenderCount />
+            </AccessDecisionManagerProvider>,
+          );
+        });
+
+        expect(
+          tree.root.findByProps({ id: 'mount-counter' }).props.children,
+        ).toBe(1);
+        expect(
+          tree.root.findByProps({ id: 'context-value' }).props.children,
+        ).toBe(JSON.stringify(updatedADM));
+      });
+    });
+  });
 });
--- packages/access-decision-manager-react/src/__tests__/is-granted-hook.test.tsx
+++ packages/access-decision-manager-react/src/__tests__/is-granted-hook.test.tsx
@@ -1,9 +1,13 @@
-import {renderHook} from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks';
 import * as React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { mocked } from 'ts-jest/utils';
 import AccessDecisionManager from '@wizeline/access-decision-manager';
-import useIsGranted, {initialState, reducer} from '../is-granted-hook';
+import useIsGranted, {
+  errorMessage,
+  initialState,
+  reducer,
+} from '../is-granted-hook';
 import AccessDecisionManagerProvider from '../access-decision-manager-provider';
 
 jest.mock('@wizeline/access-decision-manager');
@@ -14,84 +18,134 @@ describe('src', () => {
       describe('useIsGranted', () => {
         beforeEach(() => {
           mocked(AccessDecisionManager).mockClear();
-        })
-        it('a request that is granted returns `{isGranted: true, loading: false}`',  async() => {
+        });
 
+        it('a request that is granted returns `{isGranted: true, loading: false}`', async () => {
           const mockIsGrantedFn = jest.fn();
-          AccessDecisionManager.prototype.isGranted = mockIsGrantedFn
+          AccessDecisionManager.prototype.isGranted = mockIsGrantedFn;
           mockIsGrantedFn.mockReturnValue(Promise.resolve(true));
 
-          const wrapper = ({children}) => <AccessDecisionManagerProvider user={{}} voters={[]}>{children}</AccessDecisionManagerProvider>
-          const { result, waitForNextUpdate } = renderHook(() => useIsGranted('SOME'), {wrapper})
+          const wrapper = ({ children }: { children?: React.ReactNode }) => (
+            <AccessDecisionManagerProvider user={{}} voters={[]}>
+              {children}
+            </AccessDecisionManagerProvider>
+          );
+          const { result, waitForNextUpdate } = renderHook(
+            () => useIsGranted('SOME'),
+            { wrapper },
+          );
 
-          expect(result.current).toEqual({ error: undefined, isGranted: undefined, loading: true })
+          expect(result.current).toEqual({
+            error: null,
+            isGranted: null,
+            loading: true,
+          });
 
-          await waitForNextUpdate()
+          await waitForNextUpdate();
 
-          expect(result.current).toEqual({ error: undefined, isGranted: true, loading: false })
-        })
+          expect(result.current).toEqual({
+            error: null,
+            isGranted: true,
+            loading: false,
+          });
+        });
 
-        it('a request that is not granted returns `{isGranted: false, loading: false}`',  async() => {
+        it('a request that is not granted returns `{isGranted: false, loading: false}`', async () => {
           const mockIsGrantedFn = jest.fn();
-          AccessDecisionManager.prototype.isGranted = mockIsGrantedFn
+          AccessDecisionManager.prototype.isGranted = mockIsGrantedFn;
           mockIsGrantedFn.mockReturnValue(Promise.resolve(false));
 
-          const wrapper = ({children}) => <AccessDecisionManagerProvider user={{}} voters={[]}>{children}</AccessDecisionManagerProvider>
-          const { result, waitForNextUpdate } = renderHook(() => useIsGranted('SOME'), {wrapper})
+          const wrapper = ({ children }: { children?: React.ReactNode }) => (
+            <AccessDecisionManagerProvider user={{}} voters={[]}>
+              {children}
+            </AccessDecisionManagerProvider>
+          );
+          const { result, waitForNextUpdate } = renderHook(
+            () => useIsGranted('SOME'),
+            { wrapper },
+          );
 
-          expect(result.current).toEqual({ error: undefined, isGranted: undefined, loading: true });
+          expect(result.current).toEqual({
+            error: null,
+            isGranted: null,
+            loading: true,
+          });
 
           await waitForNextUpdate();
 
-          expect(result.current).toEqual({ error: undefined, isGranted: false, loading: false });
-        })
+          expect(result.current).toEqual({
+            error: null,
+            isGranted: false,
+            loading: false,
+          });
+        });
 
-        it('returns an error state when some error occurs`',  async() => {
+        it('returns an error state when some error occurs`', async () => {
           const error = new Error('Uhh oh');
           const mockIsGrantedFn = jest.fn();
-          AccessDecisionManager.prototype.isGranted = mockIsGrantedFn
+          AccessDecisionManager.prototype.isGranted = mockIsGrantedFn;
           mockIsGrantedFn.mockReturnValue(Promise.reject(error));
 
-          const wrapper = ({children}) => <AccessDecisionManagerProvider user={{}} voters={[]}>{children}</AccessDecisionManagerProvider>
-          const { result, waitForNextUpdate } = renderHook(() => useIsGranted('SOME'), {wrapper})
+          const wrapper = ({ children }: { children?: React.ReactNode }) => (
+            <AccessDecisionManagerProvider user={{}} voters={[]}>
+              {children}
+            </AccessDecisionManagerProvider>
+          );
+          const { result, waitForNextUpdate } = renderHook(
+            () => useIsGranted('SOME'),
+            { wrapper },
+          );
 
-          expect(result.current).toEqual({ error: undefined, isGranted: undefined, loading: true });
+          expect(result.current).toEqual({
+            error: null,
+            isGranted: null,
+            loading: true,
+          });
 
           await waitForNextUpdate();
 
-          expect(result.current).toEqual({ error, isGranted: undefined, loading: false });
-        })
-      })
+          expect(result.current).toEqual({
+            error,
+            isGranted: null,
+            loading: false,
+          });
+        });
+
+        it('throws error when used outside AccessDecisionManagerProvider', () => {
+          const { result } = renderHook(() => useIsGranted('SOME'));
+          expect(result.error).toStrictEqual(Error(errorMessage));
+        });
+      });
 
       describe('reducer', () => {
         it('returns the initial state for `request` type', () => {
-          const requestAction = {type: 'request'};
+          const requestAction = { type: 'request' };
           const requestedState = reducer(initialState, requestAction);
-          expect(requestedState).toEqual(initialState)
-        })
+          expect(requestedState).toEqual(initialState);
+        });
 
         it('returns error state for `error` type', () => {
           const error = new Error('Uh ohh');
-          const errorAction = {type: 'error', error};
+          const errorAction = { type: 'error', error };
           const errorState = reducer(initialState, errorAction);
           expect(errorState).toEqual({
             error,
-            isGranted: undefined,
+            isGranted: null,
             loading: false,
-          })
-        })
+          });
+        });
 
         it('returns response state for `response` type', () => {
           const isGranted = true;
-          const responseAction =  {type: 'response', isGranted};
+          const responseAction = { type: 'response', isGranted };
           const responseState = reducer(initialState, responseAction);
           expect(responseState).toEqual({
-            error: undefined,
+            error: null,
             isGranted,
             loading: false,
-          })
-        })
-      })
-    })
-  })
-})
+          });
+        });
+      });
+    });
+  });
+});
