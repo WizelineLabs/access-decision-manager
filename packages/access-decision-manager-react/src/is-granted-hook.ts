9d6db053a15e2d76d469d85e4cdb624190d83358
alfredo.granja <ext-a.granja@rappi.com>
2021-10-25 14:41:28 -0500

    updated is granted hook

--- packages/access-decision-manager-react/src/is-granted-hook.ts
+++ packages/access-decision-manager-react/src/is-granted-hook.ts
@@ -1,9 +1,12 @@
-import {useContext, useEffect, useReducer} from 'react';
-import {accessDecisionManagerContext} from './access-decision-manager-provider';
+import { useContext, useEffect, useReducer } from 'react';
+import { AccessDecisionManagerContext } from './access-decision-manager-provider';
+
+export const errorMessage =
+  "'useIsGranted' hook must be rendered within the 'AccessDecisionManagerProvider' provider";
 
 export const initialState = {
-  error: undefined,
-  isGranted: undefined,
+  error: null,
+  isGranted: null,
   loading: true,
 };
 
@@ -12,14 +15,14 @@ export const reducer = (state: any, action: any) => {
     case 'error':
       return {
         error: action.error,
-        isGranted: undefined,
+        isGranted: null,
         loading: false,
       };
     case 'request':
       return initialState;
     case 'response':
       return {
-        error: undefined,
+        error: null,
         isGranted: action.isGranted,
         loading: false,
       };
@@ -27,29 +30,30 @@ export const reducer = (state: any, action: any) => {
     default:
       throw new Error('invalid action type');
   }
-}
+};
 
-const useIsGranted = (attribute:any, subject?: any) => {
-    const  context = useContext(accessDecisionManagerContext);
-    const [state, dispatch] = useReducer(reducer, initialState);
-    useEffect
-    ( () => {
-            const isGrantedDispatcher = async () => {
-                dispatch({ type: 'request'});
+const useIsGranted = (attribute: any, subject?: any) => {
+  const accessDecisionManager = useContext(AccessDecisionManagerContext);
+  if (!accessDecisionManager) throw Error(errorMessage);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    const isGrantedDispatcher = async () => {
+      dispatch({ type: 'request' });
 
-                try{
-                    const isGranted = await context.accessDecisionManager.isGranted(attribute, subject);
-                    dispatch({type: 'response', isGranted})
-                } catch (error) {
-                    dispatch({type: 'error', error})
-                }
-            };
-            isGrantedDispatcher();
-        },
-        [context.accessDecisionManager, attribute, subject]
-    );
+      try {
+        const isGranted = await accessDecisionManager.isGranted(
+          attribute,
+          subject,
+        );
+        dispatch({ type: 'response', isGranted });
+      } catch (error) {
+        dispatch({ type: 'error', error });
+      }
+    };
+    isGrantedDispatcher();
+  }, [accessDecisionManager, attribute, subject]);
 
-    return state;
+  return state;
 };
 
 export default useIsGranted;
